a004a2ba4cacb0f26d7ca8d595ae15d2baffe7e2
Bogdan-1999 <111132034+Bogdan-1999@users.noreply.github.com>
2023-01-24 16:14:19 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -6,6 +6,24 @@ searchArea.addEventListener('keydown', event => {
     if ( event.code == 'Enter' ) {
     start();}})
 
+function openInfoPers (pers) {
+    var infoPers = document.createElement('div');
+    var searchResults = document.querySelector('.searchResults');
+    var persText = document.querySelector('.pers');
+    console.log(pers);
+    infoPers.innerHTML = 'Name: ' + pers.name + "<br>Height: " + pers.height + "<br>Hair color: " + pers.hair_color;
+    searchResults.appendChild(infoPers);
+    infoPers.setAttribute('hidden', 'true');
+    infoPers.className = 'infoPers';
+    var textClass = document.querySelector("." +infoPers.className);
+    console.log(textClass);
+    persText.addEventListener('click', function (pers) {
+        textClass.removeAttribute('hidden');
+    });
+}
+
+
+
 function start () { 
     var searchResults = document.querySelector('.searchResults');
     searchResults.innerHTML = '';
@@ -23,12 +41,14 @@ function start () {
             if (otvet.results.length == 0) {alert('Ничего не найдено')}
             for (let index = 0; index < otvet.results.length; index++) {
                 var results = document.createElement('li');
+                results.className = 'pers';
                 var searchResults = document.querySelector('.searchResults');
                 results.innerHTML = otvet.results[index].name;
                 searchResults.appendChild(results);
+                openInfoPers(otvet.results[index]);
                 searchArea.value = '';
             }
          }).catch((err) => {  
-            alert('Fetch Error :-S', err);  
+            console.log('Fetch Error :-S', err);  
           })
 }
